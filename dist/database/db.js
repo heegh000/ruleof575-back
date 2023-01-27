7b07c77c4629ae20d2ed283f1e1f66b8c73eea2e
dewpe000 <dewpe000@naver.com>
2023-01-27 09:46:05 +0000

    Add dist to gitignore

--- dist/database/db.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.db = void 0;
-const pg_1 = require("pg");
-const fs_1 = require("fs");
-const db_info = (0, fs_1.readFileSync)('./src/database/db_info.json', 'utf8');
-const db_config = JSON.parse(db_info).db_config;
-const db = new pg_1.Client(db_config);
-exports.db = db;
--- dist/database/tables.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.table_names = void 0;
-const fs_1 = require("fs");
-const db_info = (0, fs_1.readFileSync)('./src/database/db_info.json', 'utf8');
-const table_names = JSON.parse(db_info).table_names;
-exports.table_names = table_names;
